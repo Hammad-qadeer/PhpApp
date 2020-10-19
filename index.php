7305b22c126115dc654421da3d6af5ce3404df36
Hamma-qadeer <hammadqadeer12@gmail.com>
2020-10-19 17:07:00 +0500

    first-commit

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Ready Guard Security Services</title>
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+<link href='http://fonts.googleapis.com/css?family=Ruda:400,700,900' rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+<script type="text/javascript" src="scripts/jquery-1.4.3.min.js"></script>
+<script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+<script type="text/javascript" src="js/js_func.js"></script>
+
+
+<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine2/style.css" />
+<script type="text/javascript" src="engine2/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+</head>
+
+<body bgcolor="images/image1.jpg">
+
+
+<div id="main">
+
+<div id="header">
+<div class="logo"><a href="index.php"></a></div>
+<div class="menu-holder">
+<div class="contents-top">
+
+<span class="flt-lft"><img src="data/RGSS_text.png" alt="Ready Guard Security Services" /></span>
+<div class="contacts flt-ryt"></div>
+
+
+</div>
+
+<div class="menu">
+
+<ul>
+    <li class="selected"><a href="index.php" title="HOME">Home</a></li>
+    <li class="sep"></li>
+    <li><a href="aboutus.php" title="ABOUT US">About Us</a></li>
+    <li class="sep"></li>
+    <li><a href="services.php" title="SERVICES">Services</a></li>
+    <li class="sep"></li>
+    <li><a href="clients.php" title="CLIENTS">Clients</a></li>
+    <li class="sep"></li>
+    <li><a href="contactus.php" title="CONTACT US">Contact Us</a></li>
+</ul>
+
+</div><!-- menu ends here -->
+
+</div>
+</div>
+
+<div class="clear"></div>
+
+</div><!-- header -->
+
+<div class="clear">
+
+</div>
+
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+<li><img src="images/image1.jpg" alt="Pro-Active" title="Pro-Active" id="wows1_0"/>We conquer all challenges and opportunities to maintain a competitive edge.</li>
+<li><img src="images/image2.jpg" alt="Reliable" title="Reliable" id="wows1_1"/>We continuously improve our effectiveness and efficiency.</li>
+<li><img src="images/image3.jpg" alt="Professional" title="Professional" id="wows1_2"/>We provide excellence in every function that we deliver.</li>
+<li><img src="images/image4.jpg" alt="angularjs slider" title="Competent" id="wows1_3"/></a>We commit our skills and abilities to ensure your peace-of-mind.</li>
+<li><img src="images/image5.jpg" alt="Honorable" title="Honorable" id="wows1_4"/>We maintain the highest degree of integrity and honesty.</li>
+	</ul></div>
+	
+	<div class="ws_bullets"><div>
+		<a href="#" title="Pro-Active"><span>1</span></a>
+		<a href="#" title="Reliable"><span>2</span></a>
+		<a href="#" title="Professional"><span>3</span></a>
+		<a href="#" title="Competent"><span>4</span></a>
+		<a href="#" title="Honorable"><span>5</span></a>
+	</div></div><div class="ws_script" style="position:absolute;left:-0%"></div>
+	
+	<div class="ws_shadow"></div>
+	
+	</div>	
+	<script type="text/javascript" src="engine2/wowslider.js"></script>
+	<script type="text/javascript" src="engine2/script.js"></script>
+
+
+<!--banner ends here -->
+
+<div class="clear"></div>
+<div class="content">  
+
+<br>
+<div class="column_left">
+<h1 class="title_bg">Welcome to Ready Guard Security Services Pakistan</h1>
+<p>&nbsp;</p>
+
+
+<h4>Clients of RGSS will have the highest standard of manned guarding service at unbeatable price. 
+Today’s business environment requires both high tech and hands-on approach to security services and RGSS can provide your business with a security audit, 
+evaluating how secure your property and your people are. We will evaluate your facility and your procedures, that would identify the Strengths, Weaknesses, Opportunities, & Threats which will help avoid any present & future security breach. Throughout our operations, 
+our clients can rely on our honest and transparent approach to every business activity.</h4>
+</p>
+
+<p>&nbsp;</p>
+<p><img src="./images/newbn4.jpg" height="250" width="700"/></p>
+
+
+<p>&nbsp;</p>
+
+
+</div> <!--LEFT COLUMN ENDS HERE -->
+
+<div class="column_right">
+<h1 class="title_bg">Our Services</h1>
+<div class="services">
+
+<ul>
+<li> <a href="services.php#bodyguards">Close Protection Team</a></li>
+<li> <a href="services.php#premises">Security Of Premises</a></li>
+<li> <a href="services.php#Cash in Transit">Cash in Transit</a></li>
+<li> <a href="services.php#For VIPs">Armored Transport</a></li>
+<li> <a href="services.php#mobile">Mobile Escorts</a></li>
+<li> <a href="services.php#surveys">Security Surveys</a></li>
+<li> <a href="services.php#event">Event Security</a></li>
+
+</ul>
+
+</div> <!--Services ends here -->
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+
+</div> <!--RIGHT COLUMN ENDS HERE -->
+
+<div class="clear"></div>
+
+</div><!--CONTENT-->
+
+<div class="clear"></div>
+</div>
+<div class="footer_bg">
+
+<div class="footer_links">
+
+<div class="col_one flt-lft">
+<p> &nbsp; </p>
+<p>Copyright &copy; 2016-2017, Ready Guard Security Services.</p>
+<p> &nbsp; </p>
+</div>
+
+<div class="col_one flt-lft">
+<p> &nbsp; </p>
+<p>&nbsp;</p>
+<p> &nbsp; </p>
+</div>
+
+<div class="col_one flt-lft">
+<p class="copyright"> Designed &amp; Developed by: <a href="#" title="Designed and Developed by Creatives.pk">RGSS IT Team</a></p>
+</div>
+<div class="clear"></div>
+</div>
+
+</div><!-- FOOTER END HERE-->
+</body>
+</html>
